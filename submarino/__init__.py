697c6ab1eb78888972e04c07930276d11b946ac7
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2019-05-03 19:26:18 -0300

    submarino exit exit()

---
+++ submarino/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from b2w import B2W
+import json
+
+codes = B2W.Get_Prod_Codes_By_Cat("https://www.submarino.com.br/categoria/informatica/notebook", 40)
+prods = B2W.Get_Properties_By_Cods(codes)
+
+file = open("products_sub.txt", "w")
+
+
+for i in prods :
+    json.dump(i, file, ensure_ascii=False, indent=4)
+    file.write("\n\n")
+
+file.close()
+
+print(prods)
